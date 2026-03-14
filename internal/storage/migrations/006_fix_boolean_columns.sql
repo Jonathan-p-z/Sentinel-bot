62f6ee3ce6f2c0f17be63dac8136a82eb9823074
Jonathan-p-z <jperez13111@gmail.com>
2026-03-14 20:01:37 +0100

    resolution crash boolean file sql

--- internal/storage/migrations/006_fix_boolean_columns.sql
+++ internal/storage/migrations/006_fix_boolean_columns.sql
@@ -1,2 +1,7 @@
+ALTER TABLE guild_settings ALTER COLUMN lockdown_enabled DROP DEFAULT;
 ALTER TABLE guild_settings ALTER COLUMN lockdown_enabled TYPE BOOLEAN USING lockdown_enabled::boolean;
+ALTER TABLE guild_settings ALTER COLUMN lockdown_enabled SET DEFAULT FALSE;
+
+ALTER TABLE guild_settings ALTER COLUMN nuke_enabled DROP DEFAULT;
 ALTER TABLE guild_settings ALTER COLUMN nuke_enabled TYPE BOOLEAN USING nuke_enabled::boolean;
+ALTER TABLE guild_settings ALTER COLUMN nuke_enabled SET DEFAULT TRUE;
